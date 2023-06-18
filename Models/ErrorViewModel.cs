0626b05696da63c73b04f4d1ebe59ce975f37bd2
ASEGENS <125376210+ASEGENS@users.noreply.github.com>
2023-06-18 16:12:27 +0100

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace B3game.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
